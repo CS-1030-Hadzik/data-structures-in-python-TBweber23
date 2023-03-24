c1bc5630214fc0693d2c64ad4d1ea19b0e5abc3f
TBweber23 <122418172+TBweber23@users.noreply.github.com>
2023-03-23 20:51:34 -0600

    Created Array file, completed tasks

---
+++ array.py
@@ -0,0 +1,53 @@
+# Problem 1
+print('Talan Bingham')
+
+# Problem 2
+MainArray = ['Ford,','Chrysler','Dodge','Ram','Jeep','Chevy','GMC']
+
+# Problem 3
+print(MainArray)
+
+# Problem 4
+print(len(MainArray))
+
+# Problem 5
+MainArray.append('Buick')
+
+# Problem 6
+print(MainArray)
+
+# Problem 7
+print(MainArray[3])
+
+# Problem 8
+MainArray.insert(2, 'Toyota')
+
+# Problem 9 
+print(MainArray)
+
+# Problem 10
+MainArray.pop(4)
+
+# Problem 11
+print(MainArray)
+
+# Problem 12
+MainArray.sort()
+
+# Problem 13
+print(MainArray)
+
+# Problem 14
+MainArray.sort(reverse = True)
+
+# Problem 15
+print(MainArray)
+
+# Problem 16
+my_array_length = (len(MainArray))
+
+# Problem 17
+array_string = "The length of my array is:"
+
+# Problem 18
+print(array_string, + my_array_length)
